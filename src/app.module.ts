d0a21ff467404cb4756d8f7b89b71db00307a168
MD Shariful Islam <masom3456@gmail.com>
2025-02-12 15:41:21 +0600

    fix port

--- src/app.module.ts
+++ src/app.module.ts
@@ -32,12 +32,13 @@ dotenv.config()
     TypeOrmModule.forRoot({
       type: 'mysql',
       host: process.env.DB_HOST || 'localhost',
-      port: 3306,
+      port: 19766,
       username: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
       autoLoadEntities: true,
       entities: [__dirname + '/**/*.entity{.ts,.js}'],
+
    
     }),
     JwtModule.register({
