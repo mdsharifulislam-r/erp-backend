f8f1613fc447e92e9b079f606927203657871303
MD Shariful Islam <79306902+mdsharifulislam-r@users.noreply.github.com>
2025-03-27 12:17:58 +0600

    update port

--- src/app.module.ts
+++ src/app.module.ts
@@ -32,7 +32,7 @@ dotenv.config()
     TypeOrmModule.forRoot({
       type: 'mysql',
       host: process.env.DB_HOST || 'localhost',
-      port: 19766,
+      port: 3036,
       username: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
